4c40445a0100d6014ad143a87a25f4cd49e2898e
Franklinxd3000 <gentilezaxd@gmail.com>
2021-01-19 21:33:15 -0400

    Adds jsx condition to show voted items

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
 
 function Main() {
+  const [voted, setVoted] = useState(false)
   const [items, setItems] = useState([{title:'item 1', votes: 0}, {title:'item 2', votes: 3}])
 
   function handleAddVote(itemVoted){
+    setVoted(true)
     setItems(
       items.map((item)=>{
         if(item.title === itemVoted){
@@ -24,8 +26,10 @@ function Main() {
           {items.map(item =>{
             return (
               <li key={item.title}>
-                <h2> {item.title} has {item.votes} votes </h2>
-                <button onClick={()=> handleAddVote(item.title)}>Pressione para votar</button>
+                <h2> {item.title} </h2>
+                {voted ? 
+                  (<button >{item.votes}</button>):
+                  (<button onClick={()=> handleAddVote(item.title)}>Pressione para votar</button>)}
               </li>
             )
           })}
